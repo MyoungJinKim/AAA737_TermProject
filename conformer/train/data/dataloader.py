eeb0f4b30df6ef20bb37d7c090aec80195ecb8d3
anna <imannamj@gmail.com>
2025-11-21 02:32:46 +0900

    Data load 시 sampler 변수 추가

--- conformer/train/data/dataloader.py
+++ conformer/train/data/dataloader.py
@@ -1,9 +1,9 @@
 # /train/data/dataloader.py
 
 from __future__ import annotations
-from typing import Dict
+from typing import Dict, Optional
 
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, Sampler
 
 from tokenizer import TextTokenizer
 from .features import LogMelFeatureExtractor
@@ -28,7 +28,13 @@ def build_dataset(cfg: Dict, tokenizer: TextTokenizer, feature_extractor: LogMel
     )
 
 
-def build_dataloader(dataset: Dataset, collate_fn, loader_cfg: Dict, shuffle: bool):
+def build_dataloader(
+    dataset: Dataset,
+    collate_fn,
+    loader_cfg: Dict,
+    shuffle: bool,
+    sampler: Optional[Sampler] = None,
+):
     kwargs = {
         "batch_size": loader_cfg["batch_size"],
         "num_workers": loader_cfg["num_workers"],
@@ -40,5 +46,10 @@ def build_dataloader(dataset: Dataset, collate_fn, loader_cfg: Dict, shuffle: bo
 
     if loader_cfg["num_workers"] > 0 and loader_cfg.get("prefetch_factor"):
         kwargs["prefetch_factor"] = loader_cfg["prefetch_factor"]
+
+    if sampler is not None:
+        # Sampler는 shuffle과 동시에 사용할 수 없으므로 우선 적용
+        kwargs["sampler"] = sampler
+        shuffle = False
 
     return DataLoader(dataset, shuffle=shuffle, **kwargs)
